a6f89447d0e470dd0d90bdde32458d3194137e62
Rodney Folz <rodney@rodneyfolz.com>
2016-02-28 22:28:00 -0800

    Support callbacks in chrome storage shim.

--- browsers/chrome.js
+++ browsers/chrome.js
@@ -7,13 +7,13 @@ require('file?name=icon48.png!../assets/icon48.png');
 require('file?name=icon128.png!../assets/icon128.png');
 
 module.exports = {
-    loadSettings: function (callback) {
+    loadSettings: (cb) => {
         chrome.storage.sync.get(null, (data) => {
-            callback(data);
+            cb(data);
         });
     },
 
-    saveSettings: function (data) {
-        chrome.storage.sync.set(data);
+    saveSettings: (data, cb) => {
+        chrome.storage.sync.set(data, cb);
     }
 };
