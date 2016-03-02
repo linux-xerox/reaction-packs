828a9e04df429eb1e4cc9cfb3356fa70f44099cb
Rodney Folz <rodney@rodneyfolz.com>
2016-03-01 20:26:47 -0800

    Since listener is set in a callback, only trigger response once.

--- browsers/firefox.js
+++ browsers/firefox.js
@@ -8,13 +8,13 @@ require('file?name=icon48.png!../assets/icon48.png');
 
 module.exports = {
     loadSettings(cb) {
-        self.port.on('settings-loaded', (message) => {
+        self.port.once('settings-loaded', (message) => {
             cb(message);
         });
         self.port.emit('load-settings');
     },
     saveSettings(data, cb) {
-        self.port.on('settings-saved', () => {
+        self.port.once('settings-saved', () => {
             cb()
         });
         self.port.emit('save-settings', data);
