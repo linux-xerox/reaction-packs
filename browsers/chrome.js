a979f1b9014d2bba61524a687f710ed633582cf5
Rodney Folz <rodney@rodneyfolz.com>
2016-03-02 12:56:53 -0800

    Add onInstalled to chrome

--- browsers/chrome.js
+++ browsers/chrome.js
@@ -6,6 +6,15 @@ require('file?name=icon16.png!../assets/icon16.png');
 require('file?name=icon48.png!../assets/icon48.png');
 require('file?name=icon128.png!../assets/icon128.png');
 
+chrome.runtime.onInstalled.addListener((details) => {
+    if(details.reason == "install"){
+        chrome.tabs.create({url: "http://www.reactionpacks.com", active: true});
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+});
+
 module.exports = {
     loadSettings(cb) {
         chrome.storage.sync.get(null, (data) => {
