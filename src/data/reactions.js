1176eed820d4f110c9f59548e4fc911206162f51
Rodney Folz <rodney@rodneyfolz.com>
2016-04-21 16:48:28 -0700

    Fix wwwbling - still TODO hover selector

--- src/data/reactions.js
+++ src/data/reactions.js
@@ -15,7 +15,7 @@
 export default {
     "like": {
         "name": "Like",
-        "wwwBlingSelector": "._2p7a._3j7l",
+        "wwwBlingSelector": "._2p7a._3j7l, ._9--._3j7l",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_2ec001, .sp_Jnt3vJul-6Q_2x.sx_a1809d",
         "offsetBling": "0 -48px",
         "offsetLarge": "0 -144px",
@@ -24,7 +24,7 @@ export default {
     },
     "love": {
         "name": "Love",
-        "wwwBlingSelector": "._2p7a._3j7m",
+        "wwwBlingSelector": "._2p7a._3j7m, ._9--._3j7m",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_424b71, .sp_Jnt3vJul-6Q_2x.sx_d23e65",
         "offsetBling": "0 -64px",
         "offsetLarge": "0 -192px",
@@ -33,7 +33,7 @@ export default {
     },
     "haha": {
         "name": "Haha",
-        "wwwBlingSelector": "._2p7a._3j7o",
+        "wwwBlingSelector": "._2p7a._3j7o, ._9--._3j7o",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_fd2881, .sp_Jnt3vJul-6Q_2x.sx_b9c7aa",
         "offsetBling": "0 -32px",
         "offsetLarge": "0 -96px",
@@ -42,7 +42,7 @@ export default {
     },
     "wow": {
         "name": "Wow",
-        "wwwBlingSelector": "._2p7a._3j7n",
+        "wwwBlingSelector": "._2p7a._3j7n, ._9--._3j7n",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_ae4a5e, .sp_Jnt3vJul-6Q_2x.sx_aaf450",
         "offsetBling": "0 -96px",
         "offsetLarge": "0 -288px",
@@ -51,7 +51,7 @@ export default {
     },
     "sorry": {
         "name": "Sad",
-        "wwwBlingSelector": "._2p7a._3j7r",
+        "wwwBlingSelector": "._2p7a._3j7r, ._9--._3j7r",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_608018, .sp_Jnt3vJul-6Q_2x.sx_369a07",
         "offsetBling": "0 -80px",
         "offsetLarge": "0 -240px",
@@ -60,7 +60,7 @@ export default {
     },
     "anger": {
         "name": "Angry",
-        "wwwBlingSelector": "._2p7a._3j7q",
+        "wwwBlingSelector": "._2p7a._3j7q, ._9--._3j7q",
         "touchBlingSelector": ".sp_Jnt3vJul-6Q.sx_95ad2a, .sp_Jnt3vJul-6Q_2x.sx_5dc616",
         "offsetBling": "0 0",
         "offsetLarge": "0 0",
